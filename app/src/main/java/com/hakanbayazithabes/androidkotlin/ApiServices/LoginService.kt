96fc705e18eb2faf3409f445bb78795503e3491c
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-05-09 15:05:48 +0300

    Added LoginService.kt signIn()

--- app/src/main/java/com/hakanbayazithabes/androidkotlin/ApiServices/LoginService.kt
+++ app/src/main/java/com/hakanbayazithabes/androidkotlin/ApiServices/LoginService.kt
@@ -1,8 +1,10 @@
 package com.hakanbayazithabes.androidkotlin.ApiServices
 
+import com.hakanbayazithabes.androidkotlin.BuildConfig
 import com.hakanbayazithabes.androidkotlin.consts.ApiConsts
 import com.hakanbayazithabes.androidkotlin.models.ApiResponse
 import com.hakanbayazithabes.androidkotlin.models.Token
+import com.hakanbayazithabes.androidkotlin.models.UserSignIn
 import com.hakanbayazithabes.androidkotlin.models.UserSignUp
 import com.hakanbayazithabes.androidkotlin.retrofitService.ApiClient
 import com.hakanbayazithabes.androidkotlin.retrofitService.RetrofitLoginService
@@ -29,6 +31,24 @@ class LoginService {
 
             return ApiResponse(true)
 
+        }
+
+        suspend fun signIn(signIn : UserSignIn) : ApiResponse<Unit> {
+            var response = retrofitTokenServiceWithoutInterceptor.signIn(
+                BuildConfig.ClientId_ROP,
+                BuildConfig.Client_Secret_ROP,
+                ApiConsts.resourceOwnerPasswordCredentialGrantType,
+                signIn.Email,
+                signIn.Password
+            )
+
+            if (!response.isSuccessful) return ApiResponse(false)
+
+            var token = response.body() as Token
+
+            //sharedPreferences kaydedilecek
+
+            return ApiResponse(false)
         }
     }
 }
---
+++ app/src/main/java/com/hakanbayazithabes/androidkotlin/models/UserSignIn.kt
@@ -0,0 +1,4 @@
+package com.hakanbayazithabes.androidkotlin.models
+
+data class UserSignIn(var Email:String, var Password:String) {
+}
