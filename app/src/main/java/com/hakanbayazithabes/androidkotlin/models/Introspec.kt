8ce52d38d1ee768c74bbdf02c64f8bc9cd3df877
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-05-09 11:11:00 +0300

    Added RetrofitTokenService.kt

---
+++ app/src/main/java/com/hakanbayazithabes/androidkotlin/models/Introspec.kt
@@ -0,0 +1,6 @@
+package com.hakanbayazithabes.androidkotlin.models
+
+import com.google.gson.annotations.SerializedName
+
+data class Introspec(@SerializedName("active") var Active:Boolean) {
+}
---
+++ app/src/main/java/com/hakanbayazithabes/androidkotlin/retrofitService/RetrofitTokenService.kt
@@ -0,0 +1,40 @@
+package com.hakanbayazithabes.androidkotlin.retrofitService
+
+import android.media.session.MediaSession
+import com.hakanbayazithabes.androidkotlin.models.Introspec
+import com.hakanbayazithabes.androidkotlin.models.Token
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface RetrofitTokenService {
+
+    @FormUrlEncoded
+    @POST("connect/token")
+    suspend fun getTokenWithClientCredentials(
+        @Field("client_id") clientId: String,
+        @Field("client_secret") clientSecret: String,
+        @Field("grant_type") grantType: String
+    ): Response<MediaSession.Token>
+
+
+    @FormUrlEncoded
+    @POST("connect/token")
+    suspend fun CheckToken(
+        @Field("token") token: String,
+        @Header("Authorization") authorization: String
+    ) : Response<Introspec>
+
+
+    @FormUrlEncoded
+    @POST("connect/token")
+    fun refreshToken(
+        @Field("client_id") clientId: String,
+        @Field("client_secret") clientSecret: String,
+        @Field("grant_type") grantType: String,
+        @Field("refresh_token") refreshToken: String
+    ): Call<Token>
+}
