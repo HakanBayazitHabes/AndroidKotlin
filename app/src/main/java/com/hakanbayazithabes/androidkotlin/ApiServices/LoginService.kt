d9935dba2cd145bb45a6b950dc9859f7f65aa345
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-05-09 14:45:22 +0300

    Added LoginService.kt signUp()

--- app/src/main/java/com/hakanbayazithabes/androidkotlin/ApiServices/LoginService.kt
+++ app/src/main/java/com/hakanbayazithabes/androidkotlin/ApiServices/LoginService.kt
@@ -1,4 +1,34 @@
 package com.hakanbayazithabes.androidkotlin.ApiServices
 
+import com.hakanbayazithabes.androidkotlin.consts.ApiConsts
+import com.hakanbayazithabes.androidkotlin.models.ApiResponse
+import com.hakanbayazithabes.androidkotlin.models.Token
+import com.hakanbayazithabes.androidkotlin.models.UserSignUp
+import com.hakanbayazithabes.androidkotlin.retrofitService.ApiClient
+import com.hakanbayazithabes.androidkotlin.retrofitService.RetrofitLoginService
+import com.hakanbayazithabes.androidkotlin.retrofitService.RetrofitTokenService
+
 class LoginService {
+    companion object {
+        private var retrofitTokenServiceWithoutInterceptor =
+            ApiClient.builderService(ApiConsts.authBaseUrl, RetrofitLoginService::class.java, false)
+
+        suspend fun signUp(userSignUp: UserSignUp): ApiResponse<Unit> {
+            var tokenResponse = TokenService.getTokenWithClientCredentials()
+
+            if (!tokenResponse.isSuccessful) return ApiResponse(false)
+
+            var token = tokenResponse.success!!
+
+            var signUpResponse = retrofitTokenServiceWithoutInterceptor.signUp(
+                userSignUp,
+                "bearer ${token.accessToken}"
+            )
+
+            if (!signUpResponse.isSuccessful) return ApiResponse(false)
+
+            return ApiResponse(true)
+
+        }
+    }
 }
