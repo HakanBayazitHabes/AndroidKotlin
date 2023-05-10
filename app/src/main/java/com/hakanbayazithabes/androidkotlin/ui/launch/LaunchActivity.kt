8dfc32fe8d2577da90be114d36fe57226c3a5c73
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-05-11 02:34:02 +0300

    Added one part of LaunchActivity.kt and logo.png

---
+++ app/src/main/java/com/hakanbayazithabes/androidkotlin/ui/launch/LaunchActivity.kt
@@ -0,0 +1,12 @@
+package com.hakanbayazithabes.androidkotlin.ui.launch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hakanbayazithabes.androidkotlin.R
+
+class LaunchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+    }
+}
