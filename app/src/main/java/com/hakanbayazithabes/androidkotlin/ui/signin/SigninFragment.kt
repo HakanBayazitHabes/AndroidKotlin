70e3aaa2bccf6753c5128f04a0838260b167d383
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-05-16 01:53:38 +0300

    Added SigninFragment.kt -> fun signIn(),loadingState,errorState

--- app/src/main/java/com/hakanbayazithabes/androidkotlin/ui/signin/SigninFragment.kt
+++ app/src/main/java/com/hakanbayazithabes/androidkotlin/ui/signin/SigninFragment.kt
@@ -1,5 +1,6 @@
 package com.hakanbayazithabes.androidkotlin.ui.signin
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +8,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.hakanbayazithabes.androidkotlin.R
+import com.hakanbayazithabes.androidkotlin.databinding.FragmentSigninBinding
+import com.hakanbayazithabes.androidkotlin.models.UserSignIn
+import com.hakanbayazithabes.androidkotlin.ui.user.UserActivity
+import com.hakanbayazithabes.androidkotlin.utility.HelperService
+import com.hakanbayazithabes.androidkotlin.utility.LoadinState
 
 class SigninFragment : Fragment() {
 
@@ -15,18 +21,47 @@ class SigninFragment : Fragment() {
     }
 
     private lateinit var viewModel: SigninViewModel
+    private var _binding: FragmentSigninBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_signin, container, false)
+        viewModel = ViewModelProvider(this).get(SigninViewModel::class.java)
+        var root = inflater.inflate(R.layout.fragment_signin, container, false)
+        _binding = FragmentSigninBinding.bind(root)
+
+        binding.btnSignin.setOnClickListener {
+            val usersignIn = UserSignIn(
+                binding.textSigninEmail.editText?.text.toString(),
+                binding.textSigninPassword.editText?.text.toString()
+            )
+
+            viewModel.loadingState.observe(viewLifecycleOwner) {
+                when (it) {
+                    LoadinState.Loading -> binding.btnSignin.startAnimation()
+                    LoadinState.Loaded -> binding.btnSignin.revertAnimation()
+                }
+            }
+
+            viewModel.errorState.observe(viewLifecycleOwner) {
+                HelperService.showErrorMessageByToast(it)
+            }
+
+            viewModel.signIn(usersignIn).observe(viewLifecycleOwner) {
+                if (it) {
+                    var intent = Intent(requireContext(), UserActivity::class.java)
+
+                    startActivity(intent)
+                    requireActivity().finish()
+                }
+            }
+
+        }
+
+        return root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SigninViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
 }
