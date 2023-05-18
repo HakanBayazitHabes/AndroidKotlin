3892610bfcee6ce9252fb99678180e411fae645c
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-05-19 01:49:16 +0300

    Added Category.kt override Name

--- app/src/main/java/com/hakanbayazithabes/androidkotlin/models/Category.kt
+++ app/src/main/java/com/hakanbayazithabes/androidkotlin/models/Category.kt
@@ -3,7 +3,10 @@ package com.hakanbayazithabes.androidkotlin.models
 import com.google.gson.annotations.SerializedName
 
 data class Category(
-    @SerializedName("Id") var Id : Int,
-    @SerializedName("Name") var Name : String
+    @SerializedName("Id") var Id: Int,
+    @SerializedName("Name") var Name: String
 ) {
+    override fun toString(): String {
+        return Name
+    }
 }
