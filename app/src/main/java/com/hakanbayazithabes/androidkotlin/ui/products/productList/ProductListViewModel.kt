123dd023db5e0f384524ee4b27d3869169453f33
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-05-18 22:34:28 +0300

    Added method ProductListViewModel.kt changes are rolled back

--- app/src/main/java/com/hakanbayazithabes/androidkotlin/ui/products/productList/ProductListViewModel.kt
+++ app/src/main/java/com/hakanbayazithabes/androidkotlin/ui/products/productList/ProductListViewModel.kt
@@ -16,61 +16,6 @@ import com.hakanbayazithabes.androidkotlin.utility.IViewModelState
 import com.hakanbayazithabes.androidkotlin.utility.LoadinState
 import kotlinx.coroutines.launch
 
-class ProductListViewModel : ViewModel(), IViewModelState {
-    override var loadingState: MutableLiveData<LoadinState> = MutableLiveData<LoadinState>()
-    override var errorState: MutableLiveData<ApiError> = MutableLiveData<ApiError>()
-
-    @SuppressLint("NullSafeMutableLiveData")
-    fun getCategories(): LiveData<ArrayList<Category>> {
-        loadingState.value = LoadinState.Loading
-
-        var categoriesReturn = MutableLiveData<ArrayList<Category>>()
-
-        viewModelScope.launch {
-            var response = CategoryService.categoryList()
-
-            if (response.isSuccessful) {
-                categoriesReturn.value = response.success
-            } else {
-                errorState.value = response.fail
-            }
-
-            loadingState.value = LoadinState.Loaded
-
-
-        }
-
-        return categoriesReturn
-    }
-
-    @SuppressLint("NullSafeMutableLiveData")
-    fun addProduct(product: Product, fileUri: Uri?): LiveData<Product> {
-
-        loadingState.value = LoadinState.Loading
-
-        var productReturn = MutableLiveData<Product>()
-
-        viewModelScope.launch {
-            if (fileUri != null){
-                var photoResponse = PhotoService.uploadPhoto(fileUri)
-
-                if (photoResponse.isSuccessful){
-                    product.PhotoPath = photoResponse.success!!.url
-                }else{
-                    errorState.value = photoResponse.fail
-                }
-            }
-            var productResponse = ProductService.addProduct(product)
-
-            if (productResponse.isSuccessful){
-                productReturn.value = productResponse.success
-            }else{
-                errorState.value = productResponse.fail
-            }
-        }
-
-        return productReturn
-    }
-
-
+class ProductListViewModel : ViewModel() {
+    //TODO
 }
