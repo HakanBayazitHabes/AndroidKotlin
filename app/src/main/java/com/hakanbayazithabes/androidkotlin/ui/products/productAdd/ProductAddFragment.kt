05c4b750cddd8301e91ad6a345e821e2ef7e467a
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-05-19 01:48:31 +0300

    Added ProductAddFragment.kt

--- app/src/main/java/com/hakanbayazithabes/androidkotlin/ui/products/productAdd/ProductAddFragment.kt
+++ app/src/main/java/com/hakanbayazithabes/androidkotlin/ui/products/productAdd/ProductAddFragment.kt
@@ -6,7 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.lifecycle.viewModelScope
 import com.hakanbayazithabes.androidkotlin.R
+import com.hakanbayazithabes.androidkotlin.databinding.FragmentProductAddBinding
+import com.hakanbayazithabes.androidkotlin.models.Category
+import com.hakanbayazithabes.androidkotlin.utility.GlobalApp
 
 class ProductAddFragment : Fragment() {
 
@@ -15,18 +20,31 @@ class ProductAddFragment : Fragment() {
     }
 
     private lateinit var viewModel: ProductAddViewModel
+    private var _binding: FragmentProductAddBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_product_add, container, false)
+        viewModel = ViewModelProvider(this)[ProductAddViewModel::class.java]
+        var root = inflater.inflate(R.layout.fragment_product_add, container, false)
+        _binding = FragmentProductAddBinding.bind(root)
+
+        viewModel.getCategories().observe(viewLifecycleOwner) {
+            ArrayAdapter<Category>(
+                GlobalApp.getContext(),
+                android.R.layout.simple_spinner_item,
+                it
+            ).also { categoryAdapter ->
+                categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+                binding.spinnerAddFragmentCategories.adapter = categoryAdapter
+            }
+        }
+
+        return root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ProductAddViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
 }
