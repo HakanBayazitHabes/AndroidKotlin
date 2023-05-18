b9444f075ef46e590fba6998b2204d2d1c56bfc7
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-05-18 22:36:39 +0300

    Added method ProductAddViewModel.kt -> getCategories(),addProduct()

--- app/src/main/java/com/hakanbayazithabes/androidkotlin/ui/products/productAdd/ProductAddViewModel.kt
+++ app/src/main/java/com/hakanbayazithabes/androidkotlin/ui/products/productAdd/ProductAddViewModel.kt
@@ -1,7 +1,74 @@
 package com.hakanbayazithabes.androidkotlin.ui.products.productAdd
 
+import android.annotation.SuppressLint
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hakanbayazithabes.androidkotlin.ApiServices.CategoryService
+import com.hakanbayazithabes.androidkotlin.ApiServices.PhotoService
+import com.hakanbayazithabes.androidkotlin.ApiServices.ProductService
+import com.hakanbayazithabes.androidkotlin.models.ApiError
+import com.hakanbayazithabes.androidkotlin.models.Category
+import com.hakanbayazithabes.androidkotlin.models.Product
+import com.hakanbayazithabes.androidkotlin.utility.IViewModelState
+import com.hakanbayazithabes.androidkotlin.utility.LoadinState
+import kotlinx.coroutines.launch
 
-class ProductAddViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ProductAddViewModel : ViewModel(), IViewModelState {
+    override var loadingState: MutableLiveData<LoadinState> = MutableLiveData<LoadinState>()
+    override var errorState: MutableLiveData<ApiError> = MutableLiveData<ApiError>()
+
+    @SuppressLint("NullSafeMutableLiveData")
+    fun getCategories(): LiveData<ArrayList<Category>> {
+        loadingState.value = LoadinState.Loading
+
+        var categoriesReturn = MutableLiveData<ArrayList<Category>>()
+
+        viewModelScope.launch {
+            var response = CategoryService.categoryList()
+
+            if (response.isSuccessful) {
+                categoriesReturn.value = response.success
+            } else {
+                errorState.value = response.fail
+            }
+
+            loadingState.value = LoadinState.Loaded
+
+        }
+
+        return categoriesReturn
+    }
+
+    @SuppressLint("NullSafeMutableLiveData")
+    fun addProduct(product: Product, fileUri: Uri?): LiveData<Product> {
+
+        loadingState.value = LoadinState.Loading
+
+        var productReturn = MutableLiveData<Product>()
+
+        viewModelScope.launch {
+            if (fileUri != null) {
+                var photoResponse = PhotoService.uploadPhoto(fileUri)
+
+                if (photoResponse.isSuccessful) {
+                    product.PhotoPath = photoResponse.success!!.url
+                } else {
+                    errorState.value = photoResponse.fail
+                }
+            }
+            var productResponse = ProductService.addProduct(product)
+
+            if (productResponse.isSuccessful) {
+                productReturn.value = productResponse.success
+            } else {
+                errorState.value = productResponse.fail
+            }
+        }
+
+        return productReturn
+    }
+
 }
