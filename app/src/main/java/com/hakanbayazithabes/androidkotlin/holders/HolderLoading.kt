6ea0eb5a8de92ed7bb74afe373124a864db89d39
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-05-21 22:52:32 +0300

    Added HolderLoading.kt

---
+++ app/src/main/java/com/hakanbayazithabes/androidkotlin/holders/HolderLoading.kt
@@ -0,0 +1,7 @@
+package com.hakanbayazithabes.androidkotlin.holders
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class HolderLoading(view: View) : RecyclerView.ViewHolder(view) {
+}
