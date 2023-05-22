c77652c4576cba3103ddc63fb1f9500d468b513c
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-05-23 00:36:43 +0300

    Added getProduct(productId) in ProductDetailViewModel.kt

--- app/src/main/java/com/hakanbayazithabes/androidkotlin/ui/products/productDetail/ProductDetailViewModel.kt
+++ app/src/main/java/com/hakanbayazithabes/androidkotlin/ui/products/productDetail/ProductDetailViewModel.kt
@@ -1,7 +1,38 @@
 package com.hakanbayazithabes.androidkotlin.ui.products.productDetail
 
+import android.annotation.SuppressLint
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hakanbayazithabes.androidkotlin.ApiServices.ProductService
+import com.hakanbayazithabes.androidkotlin.models.ApiError
+import com.hakanbayazithabes.androidkotlin.models.Product
+import com.hakanbayazithabes.androidkotlin.utility.IViewModelState
+import com.hakanbayazithabes.androidkotlin.utility.LoadinState
+import kotlinx.coroutines.launch
 
-class ProductDetailViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ProductDetailViewModel : ViewModel(), IViewModelState {
+    override var loadingState: MutableLiveData<LoadinState> = MutableLiveData<LoadinState>()
+    override var errorState: MutableLiveData<ApiError> = MutableLiveData<ApiError>()
+
+    @SuppressLint("NullSafeMutableLiveData")
+    fun getProduct(productId: Int): LiveData<Product> {
+        var productReturn = MutableLiveData<Product>()
+
+        loadingState.value = LoadinState.Loading
+
+        viewModelScope.launch {
+            var response = ProductService.getProductById(productId)
+
+            if (response.isSuccessful) {
+                productReturn.value = response.success
+            } else {
+                errorState.value = response.fail
+            }
+            loadingState.value = LoadinState.Loaded
+        }
+
+        return productReturn
+    }
 }
