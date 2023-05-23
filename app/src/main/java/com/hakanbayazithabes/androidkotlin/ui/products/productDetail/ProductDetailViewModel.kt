1811602429b64c974cba0de403b0e25037cc6e46
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-05-23 14:27:01 +0300

    Added deleteProduct(id) in ProductDetailViewModel.kt

--- app/src/main/java/com/hakanbayazithabes/androidkotlin/ui/products/productDetail/ProductDetailViewModel.kt
+++ app/src/main/java/com/hakanbayazithabes/androidkotlin/ui/products/productDetail/ProductDetailViewModel.kt
@@ -35,4 +35,24 @@ class ProductDetailViewModel : ViewModel(), IViewModelState {
 
         return productReturn
     }
+
+    @SuppressLint("NullSafeMutableLiveData")
+    fun deleteProduct(productId: Int): LiveData<Boolean> {
+        var productReturn = MutableLiveData<Boolean>()
+
+        loadingState.value = LoadinState.Loading
+
+        viewModelScope.launch {
+            var response = ProductService.deleteProduct(productId)
+
+            if (response.isSuccessful) {
+                productReturn.value = true
+            } else {
+                errorState.value = response.fail
+            }
+            loadingState.value = LoadinState.Loaded
+        }
+
+        return productReturn
+    }
 }
