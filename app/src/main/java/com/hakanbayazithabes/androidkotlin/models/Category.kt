21d9517c305e7f3f84438ca6a4beb080ac5c4a76
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-05-23 15:08:59 +0300

    Added navigation argument parcalize

--- app/src/main/java/com/hakanbayazithabes/androidkotlin/models/Category.kt
+++ app/src/main/java/com/hakanbayazithabes/androidkotlin/models/Category.kt
@@ -1,11 +1,15 @@
 package com.hakanbayazithabes.androidkotlin.models
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 import com.google.gson.annotations.SerializedName
 
+@Parcelize
 data class Category(
     @SerializedName("Id") var Id: Int,
     @SerializedName("Name") var Name: String
-) {
+) : Parcelable {
     override fun toString(): String {
         return Name
     }
--- app/src/main/java/com/hakanbayazithabes/androidkotlin/models/Product.kt
+++ app/src/main/java/com/hakanbayazithabes/androidkotlin/models/Product.kt
@@ -1,7 +1,10 @@
 package com.hakanbayazithabes.androidkotlin.models
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Product(
     @SerializedName("Id") var Id: Int,
     @SerializedName("Name") var Name: String,
@@ -11,5 +14,4 @@ data class Product(
     @SerializedName("PhotoPath") var PhotoPath: String,
     @SerializedName("Category_Id") var Category_Id: Int,
     @SerializedName("Category") var Category: Category?
-) {
-}
+) : Parcelable
